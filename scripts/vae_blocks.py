eec493190e6c6d21cd3b7f1121ada5031610d43e
Henrik Forstén <henrik.forsten@gmail.com>
2023-07-14 11:00:30 +0300

    Fixes, SDXL support

--- scripts/vae_blocks.py
+++ scripts/vae_blocks.py
@@ -3,11 +3,17 @@ import torch
 def fp16_clip(self, x, *args, **kwargs):
     x_orig = x
     iters = 0
-    while iters < 5:
+    max_iters = 5
+    while True:
+        x = torch.clip(x, -self.clip_th, self.clip_th)
         x = self.orig_forward(x, *args, **kwargs)
         iters += 1
         if torch.any(torch.isnan(x)):
             self.clip_th *= self.clip_fail
+            if iters > max_iters:
+                # Failed to clip
+                print('fp16 VAE failed to clip inputs')
+                break
             x = x_orig
             continue
         else:
@@ -19,7 +25,7 @@ def nonlinearity(x):
     # swish
     return x*torch.sigmoid(x)
 
-def decoder_forward(self, z):
+def decoder_forward(self, z, **kwargs):
     self.last_z_shape = z.shape
 
     # timestep embedding
@@ -29,16 +35,16 @@ def decoder_forward(self, z):
     h = self.conv_in(z)
 
     # middle
-    h = self.mid.block_1(h, temb)
-    h = self.mid.attn_1(h)
-    h = self.mid.block_2(h, temb)
+    h = self.mid.block_1(h, temb, **kwargs)
+    h = self.mid.attn_1(h, **kwargs)
+    h = self.mid.block_2(h, temb, **kwargs)
 
     # upsampling
     for i_level in reversed(range(self.num_resolutions)):
-        for i_block in range(self.num_res_blocks+1):
-            h = self.up[i_level].block[i_block](h, temb)
+        for i_block in range(self.num_res_blocks + 1):
+            h = self.up[i_level].block[i_block](h, temb, **kwargs)
             if len(self.up[i_level].attn) > 0:
-                h = self.up[i_level].attn[i_block](h)
+                h = self.up[i_level].attn[i_block](h, **kwargs)
         if i_level != 0:
             h = self.up[i_level].upsample(h)
 
@@ -53,7 +59,7 @@ def decoder_forward(self, z):
         h = torch.clip(h, -self.clip_th, self.clip_th)
         h = self.norm_out(h)
         h = nonlinearity(h)
-        h = self.conv_out(h)
+        h = self.conv_out(h, **kwargs)
         if self.tanh_out:
             h = torch.tanh(h)
         if torch.any(torch.isnan(h)):
